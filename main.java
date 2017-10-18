af1614ed817c268449d3dd5eb234617c0b8c0f70
RGrab <rgrabar@csumb.edu>
2017-10-18 16:01:54 -0700

    removed needed imports in main class

--- main.java
+++ main.java
@@ -1,8 +1,7 @@
-import Jama.Matrix;
-
 public class main {
-  public static void main(String[] args){
+//home/ryan/java-jar-packages/Jama-1.0.3.jar
 
+  public static void main(String[] args){
     NeuralNetwork neuralNetwork = new NeuralNetwork(3,3,3,.3d);
     System.out.println("DONE!");
   }
